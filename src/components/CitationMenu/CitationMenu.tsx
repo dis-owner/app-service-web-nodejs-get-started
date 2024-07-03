950bb6c4e7911745d630cd41f7883c4621db4519
soma <ay.souma@kkdis.co.jp>
2024-07-03 15:23:36 +0900

    CitationMenu追加

--- src/components/CitationMenu/CitationMenu.tsx
+++ src/components/CitationMenu/CitationMenu.tsx
@@ -39,7 +39,8 @@ const CitationMenu = () => {
 
   //新規追加 20240529
   // コンテナ名
-  const containerName = encodeURIComponent(import.meta.env.VITE_CONTAINER_NAME?? '');
+  const containerName = import.meta.env.VITE_CONTAINER_NAME?? '';
+  console.log("containerName"+ containerName);
   // 呼び出したいファイル名
   const blobName = citationContent.title;
   console.log("citationContent.title:"+ blobName);
